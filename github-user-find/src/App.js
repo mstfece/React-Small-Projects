822dcc230bfd55925593d797c65901a8a6d04b33
mstfece <82402952+mstfece@users.noreply.github.com>
2023-06-25 23:55:09 +0300

    pushh

--- github-user-find/src/App.js
+++ github-user-find/src/App.js
@@ -25,5 +25,29 @@ export default function App() {
     const results = await fetchResults(query);
     setResults(results);
   }
-
+  return (
+    <div className="app">
+      <main className="main">
+        <h2>Project 5: GitHub User Search</h2>
+        <Form
+          onChange={onSearchChange}
+          onSubmit={onSearchSubmit}
+          value={query}
+        />
+        <h3>Results</h3>
+        <div id="results">
+          <div>
+            {results.map((user) => (
+              <User
+                key={user.login}
+                avatar={user.avatar_url}
+                url={user.html_url}
+                username={user.login}
+              />
+            ))}
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 export default App;
